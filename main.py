3f88a5f9c2846e73b505edd6ecb911186ebb3f51
Pluto130 <89603351+Pluto130@users.noreply.github.com>
2021-12-02 18:31:41 -0500

    removing score

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ def find_current_location(location_label):
 
 #
 
-def render(current_location, score, moves):
+def render(current_location, moves):
 	if "name" in current_location and "cleanText" in current_location:
-		print("Total moves: " + str(moves) + ", Score: " + str(score))
+		print("Total moves: " + str(moves))
 		print("You are located at the " + str(current_location["name"]))
 		print(current_location["cleanText"] + "\n")
 
 
 def get_input():
-	response = input("Where would you like to go? ")
+	response = input("What now? ")
 	response = response.upper().strip()
 	return response
 
@@ -38,19 +38,15 @@ def update(current_location, location_label, response):
 #############location_label = "[First Area]"
 current_location = {}
 response = ""
-score = 0
 moves = 0
 
 while True:
 	if response == "QUIT":
 		break
 	moves =+ 1
-	if "score" in current_location:
-		score = score + current_location["score"]
 	location_label = update(current_location, location_label, response)
 	current_location = find_current_location(location_label)
-	render(current_location, score, moves)
+	render(current_location, moves)
 	response = get_input()
 
 print("End")
-
